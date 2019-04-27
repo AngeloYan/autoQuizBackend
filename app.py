8063abbceb9b2c2dcb3344acf1de8eac64288ce3
kellerHuang <keller.huang98@gmail.com>
2019-04-27 21:33:58 +1000

    cors

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 import json
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/",methods=["GET","POST"])
+@cross_origin()
 def page():
     response.headers.add('Access-Control-Allow-Origin','*')
     if request.method == "POST":

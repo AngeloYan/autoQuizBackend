d88a5c5790fd1526fb0cf1ff94392ed66e473321
kellerHuang <keller.huang98@gmail.com>
2019-04-27 20:29:58 +1000

    basics

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def generateQ():
-        return "ok"
+@app.route("/",methods=["GET","POST"])
+def page():
+    if request.method == "POST":
+        x = request.get_json()
+        topic = json.loads(x)["topic"]
+        #q = getQuestions(topic)
+    return topic
